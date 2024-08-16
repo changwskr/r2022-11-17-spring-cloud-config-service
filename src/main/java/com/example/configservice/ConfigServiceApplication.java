f75498174069e374e92028c4fc14bbc8a559d691
changwskr <changwskr@naver.com>
2024-08-16 15:12:10 +0900

    11

--- src/main/java/com/example/configservice/ConfigServiceApplication.java
+++ src/main/java/com/example/configservice/ConfigServiceApplication.java
@@ -12,6 +12,11 @@ import java.util.Properties;
 @EnableConfigServer
 public class ConfigServiceApplication {
 
+
+
+    //
+
+
     public static void main(String[] args) {
         SpringApplication.run(ConfigServiceApplication.class, args);
     }
