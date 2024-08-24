80271ef4f6024b36d230d8f5ada6a7e9ce2550ed
changwskr <changwskr@naver.com>
2024-08-24 17:45:19 +0900

    2024-08-24-03

--- zdeploy-docker/docker_run-containerimg.bat
+++ zdeploy-docker/docker_run-containerimg.bat
@@ -5,5 +5,3 @@ docker run -d -p 8888:8888 --network ecommerce-network ^
     -e "spring.rabbitmq.host=rabbitmq" ^
     -e "spring.profiles.active=default" ^
     --name my-configservice changwskr/my-configservice:1.0
-
-docker ps | findstr my-configservice
